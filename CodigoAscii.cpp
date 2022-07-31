cce7f9ab1f82b1cd4bde5bc904f0eb7cfa7eaae9
EdgarYahirPinaElguero <89890681+EdgarYahirPinaElguero@users.noreply.github.com>
2022-07-30 20:05:08 -0500

    Create CodigoAscii.cpp

---
+++ CodigoAscii.cpp
@@ -0,0 +1,15 @@
+package Ejercicio8;
+import java.util.*;
+
+public class CaracteresAscii {
+	public static void main(String[] args) {
+		
+		 System.out.println("Programa que muestre los caracteres ASCII del 0 al 25:");
+		 System.out.println("-----");
+		 
+		 for(int i =0; i<=25; i++)
+	        {
+	            System.out.println( i + ". " + (char)i);
+	        }
+    }
+}
